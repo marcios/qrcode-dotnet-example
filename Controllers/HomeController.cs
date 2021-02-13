2513aa1fd21982b53cc898e138d2143c4443f07f
Marcio JF Santos <marcioj.fsantos@outlook.com>
2021-02-12 22:43:59 -0300

    init

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using QrCodeGen.Models;
+using QrCodeGen.Utils;
+using QRCoder;
+
+namespace QrCodeGen.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [HttpGet("qrcode")]
+        public IActionResult GetQrCode([FromQuery]string url)
+        {
+            var image = QrCodeManager.GenerateByteArray(url);
+            return File(image, "image/jpeg");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
